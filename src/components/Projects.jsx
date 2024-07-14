7ac70ad333ec37ce3fb9bee1f17e9b40abeb4632
Kevin-abhi <mallabhishekpratap88@gmail.com>
2024-07-14 13:55:35 +0530

    visit link change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,8 +31,9 @@ const Projects = () => {
                         transition={{ duration: 1 }} 
                         className="w-full max-w-xl lg:w-3/4">
                         <div className="flex justify-between w-full">
-                        <h6 className="mb-2 font-semibold">{project.title}</h6>
-                        <a href={project.link} className="mb-2 font-semibold">Visit</a>
+                        <h6 className="mb-2 font-semibold">
+                        <a href={project.link}>{project.title}</a></h6>
+                        {/* <a href={project.link} className="mb-2 font-semibold">Visit</a> */}
                         </div>
                         <p className="mb-4 text-neutral-400">{project.description}</p>
                         {project.technologies.map((tech, index) => (
